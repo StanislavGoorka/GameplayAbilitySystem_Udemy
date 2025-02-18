d3af062fbfae7177dd31ac8a4b1bcf4efe5ec298
StanislavGoorka <stanislaw.gorka.sg@gmail.com>
2025-02-18 21:19:13 +0100

    Player and Base Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
